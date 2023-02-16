fb6f9fb15381491a68e24fe5445c2ca3276ed860
“LEOTAO” <1299689745@qq.com>
2023-02-16 19:36:57 +0800

    feed_list first commit test

--- service/vedio/feed_video.go
+++ service/vedio/feed_video.go
@@ -2,35 +2,63 @@ package vedio
 
 import (
 	"TikTok_Project/repository"
-
 	"time"
 )
 
-//const maxVNum = 30
+const maxVNum int = 30
 
-type feedVideo struct {
-	Videos   []*repository.Video `json:"video_list,omitempty"`
-	NextTime int64               `json:"next_time,omitempty"`
+type FeedResponse struct {
+	VideoList []*repository.Video `json:"video_list,omitempty"`
+	NextTime  int64               `json:"next_time,omitempty"`
 }
 
-//
-func QueryFeedVideoList(userId int64, latestTime time.Time) (*feedVideo, error) {
-	return NewQueryFeedVideoListFlow(userId, latestTime).Do()
-}
+//func QueryFeedVideoList(userId int64, latestTime time.Time) (*FeedResponse, error) {
+//	return NewQueryFeedVideoListFlow(userId, latestTime).Do()
+//}
 
 type QueryFeedVideoListFlow struct {
 	userId     int64
 	latestTime time.Time
 	videos     []*repository.Video
 	nextTime   int64
-	feedVideo  *feedVideo
+	feedVideo  *FeedResponse
 }
 
-func NewQueryFeedVideoListFlow(userId int64, latestTime time.Time) *QueryFeedVideoListFlow {
-	return &QueryFeedVideoListFlow{userId: userId, latestTime: latestTime}
-}
+//func NewQueryFeedVideoListFlow(userId int64, latestTime time.Time) *QueryFeedVideoListFlow {
+//	return &QueryFeedVideoListFlow{userId: userId, latestTime: latestTime}
+//}
 
-func (q *QueryFeedVideoListFlow) Do() (*feedVideo, error) {
-	//所有传入的参数不填也应该给他正常处理
+func (q *QueryFeedVideoListFlow) Do() (*FeedResponse, error) {
+	q.check()
+	if err := q.packData(); err != nil {
+		return nil, err
+	}
 	return q.feedVideo, nil
+}
+func (q *QueryFeedVideoListFlow) packData() error {
+	q.feedVideo = &FeedResponse{
+		VideoList: q.videos,
+		NextTime:  q.nextTime,
+	}
+	return nil
+}
+
+func (q *QueryFeedVideoListFlow) check() {
+	//上层通过把userId置零，表示userId不存在或不需要
+	if q.userId > 0 {
+		//这里说明userId是有效的，可以定制性的做一些登录用户的专属视频推荐
+	}
+
+	if q.latestTime.IsZero() {
+		q.latestTime = time.Now()
+	}
+}
+
+func (q *QueryFeedVideoListFlow) prepareData() error {
+	err := repository.NewVideoDAO().QueryVideoListByLimitAndTime(maxVNum, q.latestTime, &q.videos)
+	if err != nil {
+		return err
+	}
+	q.nextTime = time.Now().Unix() / 1e6
+	return nil
 }
