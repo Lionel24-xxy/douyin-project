5703f9951f151ff746e25eef9f1bf5f0040f9f94
zhouxing9454 <zhouxing9454@gmail.com>
2023-02-20 15:08:37 +0800

    dev-2023-02-20-增加feed路由

--- router/router.go
+++ router/router.go
@@ -18,6 +18,7 @@ func InitRouter() *gin.Engine {
 	uGroup := r.Group("douyin")
 	{
 		// 基础接口
+		uGroup.GET("/feed/", feed.FeedVideoHandler)
 		uGroup.POST("/user/register/", user.UserRegister)
 		uGroup.POST("/user/login/", user.UserLogin)
 		uGroup.GET("/user/", middleware.JWTMiddleWare(), user.UserInfo)
