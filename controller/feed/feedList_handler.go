1b554d61e18b405f50e590286f5159a2886c68e1
zhouxing9454 <zhouxing9454@gmail.com>
2023-02-20 15:09:15 +0800

    dev-2023-02-20-视频流接口实现

---
+++ controller/feed/feedList_handler.go
@@ -0,0 +1,109 @@
+package feed
+
+import (
+	"TikTok_Project/service/video"
+	"TikTok_Project/utils"
+	"errors"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+type FeedResponse struct {
+	StatusCode int32  `json:"status_code"`
+	StatusMsg  string `json:"status_msg,omitempty"`
+	*video.FeedVideoList
+}
+
+type ProxyFeedVideoList struct {
+	*gin.Context
+}
+
+func NewProxyFeedVideoList(ctx *gin.Context) *ProxyFeedVideoList {
+	return &ProxyFeedVideoList{ctx}
+}
+
+func (p *ProxyFeedVideoList) FeedVideoListError(msg string) {
+	p.JSON(http.StatusOK,
+		FeedResponse{
+			StatusCode: 1,
+			StatusMsg:  msg,
+		})
+}
+
+func (p *ProxyFeedVideoList) FeedVideoListOK(videoList *video.FeedVideoList) {
+	p.JSON(http.StatusOK,
+		FeedResponse{
+			StatusCode:    0,
+			StatusMsg:     "success!",
+			FeedVideoList: videoList,
+		})
+}
+
+func FeedVideoHandler(ctx *gin.Context) {
+	p := NewProxyFeedVideoList(ctx)
+	token, ok := ctx.GetQuery("token")
+
+	//无登录状态
+	if !ok {
+		err := p.DoNoToken()
+		if err != nil {
+			p.FeedVideoListError(err.Error())
+		}
+		return
+	}
+
+	//有登录状态
+	err := p.DoHasToken(token)
+	if err != nil {
+		p.FeedVideoListError(err.Error())
+	}
+}
+
+// DoNoToken 未登录的视频流推送处理
+func (p *ProxyFeedVideoList) DoNoToken() error {
+	rawTimestamp := p.Query("latest_time")
+	var latestTime time.Time
+	intTime, err := strconv.ParseInt(rawTimestamp, 10, 64)
+	if err == nil {
+		latestTime = time.Unix(0, intTime*1e6) ////注意：前端传来的时间戳是以ms为单位的
+	}
+	videoList, err := video.QueryFeedVideoList(0, latestTime)
+	if err != nil {
+		return err
+	}
+	p.FeedVideoListOK(videoList)
+	return nil
+}
+
+// DoHasToken 如果是登录状态，则生成UserId字段
+func (p *ProxyFeedVideoList) DoHasToken(token string) error {
+	//解析成功
+	if claim, ok := utils.ParseToken(token); ok {
+		//token超时
+		if time.Now().Unix() > claim.ExpiresAt {
+			return errors.New("token超时")
+		}
+		rawTimestamp := p.Query("latest_time")
+		var latestTime time.Time
+		intTime, err := strconv.ParseInt(rawTimestamp, 10, 64)
+		if err != nil {
+			latestTime = time.Unix(0, intTime*1e6) //注意：前端传来的时间戳是以ms为单位的
+		}
+		//调用service层接口
+		videoList, err := video.QueryFeedVideoList(claim.UserId, latestTime)
+		if err != nil {
+			return err
+		}
+		p.FeedVideoListOK(videoList)
+		return nil
+	}
+	//解析失败
+	return errors.New("token不正确")
+}
+
+//            "favorite_count": 0,
+//            "comment_count": 0,
+//            "is_favorite": true,
+//这之后要补这三个的
--- service/video/feed_video.go
+++ service/video/feed_video.go
@@ -5,15 +5,14 @@ import (
 	"time"
 )
 
-const maxVNum int = 30
+// MaxVideoNum 每次最多返回的视频流数量
+const (
+	MaxVideoNum = 30
+)
 
 type FeedVideoList struct {
-	VideoList []*repository.Video `json:"video_list,omitempty"`
-	NextTime  int64               `json:"next_time,omitempty"`
-}
-
-func QueryFeedVideoList(userId int64, latestTime time.Time) (*FeedVideoList, error) {
-	return NewQueryFeedVideoListFlow(userId, latestTime).Do()
+	Videos   []*repository.Video `json:"video_list,omitempty"`
+	NextTime int64               `json:"next_time,omitempty"`
 }
 
 type QueryFeedVideoListFlow struct {
@@ -29,24 +28,6 @@ func NewQueryFeedVideoListFlow(userId int64, latestTime time.Time) *QueryFeedVid
 }
 
 func (q *QueryFeedVideoListFlow) Do() (*FeedVideoList, error) {
-	q.check()
-	if err := q.prepareData(); err != nil {
-		return nil, err
-	}
-	if err := q.packData(); err != nil {
-		return nil, err
-	}
-	return q.feedVideo, nil
-}
-func (q *QueryFeedVideoListFlow) packData() error {
-	q.feedVideo = &FeedVideoList{
-		VideoList: q.videos,
-		NextTime:  q.nextTime,
-	}
-	return nil
-}
-
-func (q *QueryFeedVideoListFlow) check() {
 	//上层通过把userId置零，表示userId不存在或不需要
 	if q.userId > 0 {
 		//这里说明userId是有效的，可以定制性的做一些登录用户的专属视频推荐
@@ -55,13 +36,33 @@ func (q *QueryFeedVideoListFlow) check() {
 	if q.latestTime.IsZero() {
 		q.latestTime = time.Now()
 	}
+	if err := q.prepareData(); err != nil {
+		return nil, err
+	}
+	if err := q.packData(); err != nil {
+		return nil, err
+	}
+	return q.feedVideo, nil
 }
 
 func (q *QueryFeedVideoListFlow) prepareData() error {
-	err := repository.NewVideoDAO().QueryVideoListByLimitAndTime(maxVNum, q.latestTime, &q.videos)
+	err := repository.NewVideoDAO().QueryVideoListByLimitAndTime(MaxVideoNum, q.latestTime, &q.videos)
 	if err != nil {
 		return err
 	}
+	//还有要补充的
 	q.nextTime = time.Now().Unix() / 1e6
 	return nil
+}
+
+func (q *QueryFeedVideoListFlow) packData() error {
+	q.feedVideo = &FeedVideoList{
+		Videos:   q.videos,
+		NextTime: q.nextTime,
+	}
+	return nil
+}
+
+func QueryFeedVideoList(userId int64, latestTime time.Time) (*FeedVideoList, error) {
+	return NewQueryFeedVideoListFlow(userId, latestTime).Do()
 }
