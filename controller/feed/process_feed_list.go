c14ae06365a0feffa824ac636cb5d9de82fa26e0
“LEOTAO” <1299689745@qq.com>
2023-02-20 17:27:56 +0800

    获取用户粉丝列表

--- controller/feed/process_feed_list.go
+++ controller/feed/process_feed_list.go
@@ -1,21 +1,13 @@
 package feed
 
-import (
-	"TikTok_Project/service/video"
-	"github.com/gin-gonic/gin"
-	"net/http"
-	"strconv"
-	"time"
-)
-
-type FeedResponse struct {
-	StatusCode int32  `json:"status_code"`
-	StatusMsg  string `json:"status_msg,omitempty"`
-}
-
-type ProxyFeedVideoList struct {
-	*gin.Context
-}
+//type FeedResponse struct {
+//	StatusCode int32  `json:"status_code"`
+//	StatusMsg  string `json:"status_msg,omitempty"`
+//}
+//
+//type ProxyFeedVideoList struct {
+//	*gin.Context
+//}
 
 //func ProcessFeedVideoList(c *gin.Context) {
 //	p := NewProxyFeedVideoList(c)
@@ -30,36 +22,36 @@ type ProxyFeedVideoList struct {
 //
 //}
 
-func NewProxyFeedVideoList(c *gin.Context) *ProxyFeedVideoList {
-	return &ProxyFeedVideoList{Context: c}
-}
-
-// DoNoLog 未登录的视频流推送处理
-func (p *ProxyFeedVideoList) DoNoLog() error {
-	rawTimestamp := p.Query("latest_time")
-	var latestTime time.Time
-	intTime, err := strconv.ParseInt(rawTimestamp, 10, 64)
-	if err == nil {
-		latestTime = time.Unix(0, intTime*1e6) //注意：前端传来的时间戳是以ms为单位的
-	}
-	videoList, err := video.QueryFeedVideoList(0, latestTime)
-	if err != nil {
-		return err
-	}
-	p.FeedVideoListOk(videoList)
-	return nil
-}
-
-func (p *ProxyFeedVideoList) FeedVideoListError(msg string) {
-	p.JSON(http.StatusOK, FeedResponse{
-		StatusCode: 1,
-		StatusMsg:  msg,
-	})
-}
-
-func (p *ProxyFeedVideoList) FeedVideoListOk(videoList *video.FeedVideoList) {
-	p.JSON(http.StatusOK, FeedResponse{
-		StatusCode: 0,
-	},
-	)
-}
+//func NewProxyFeedVideoList(c *gin.Context) *ProxyFeedVideoList {
+//	return &ProxyFeedVideoList{Context: c}
+//}
+//
+//// DoNoLog 未登录的视频流推送处理
+//func (p *ProxyFeedVideoList) DoNoLog() error {
+//	rawTimestamp := p.Query("latest_time")
+//	var latestTime time.Time
+//	intTime, err := strconv.ParseInt(rawTimestamp, 10, 64)
+//	if err == nil {
+//		latestTime = time.Unix(0, intTime*1e6) //注意：前端传来的时间戳是以ms为单位的
+//	}
+//	videoList, err := video.QueryFeedVideoList(0, latestTime)
+//	if err != nil {
+//		return err
+//	}
+//	p.FeedVideoListOk(videoList)
+//	return nil
+//}
+//
+//func (p *ProxyFeedVideoList) FeedVideoListError(msg string) {
+//	p.JSON(http.StatusOK, FeedResponse{
+//		StatusCode: 1,
+//		StatusMsg:  msg,
+//	})
+//}
+//
+//func (p *ProxyFeedVideoList) FeedVideoListOk(videoList *video.FeedVideoList) {
+//	p.JSON(http.StatusOK, FeedResponse{
+//		StatusCode: 0,
+//	},
+//	)
+//}
--- repository/user.go
+++ repository/user.go
@@ -5,23 +5,27 @@ import (
 	"sync"
 )
 
+var (
+	EmptyUserList = errors.New("用户粉丝列表为空")
+)
+
 type User struct {
-	ID 					int64 		`json:"id" gorm:"id,omitempty;primaryKey;AUTO_INCREMENT"`
-	Username 			string 		`json:"username" gorm:"username,omitempty"`
-	Password 			string 		`json:"password" gorm:"size:200;notnull"`
-	FollowCount 		int64 		`json:"follow_count" gorm:"follow_count,omitempty"`
-	FollowerCount 		int64       `json:"follower_count" gorm:"follower_count,omitempty"`
-	IsFollow      		bool        `json:"is_follow" gorm:"is_follow,omitempty"`
-	Avatar				string		`json:"avatar" gorm:"avatar,omitempty"`
-	BackgroundImage		string		`json:"background_image" gorm:"background_image,omitempty"`
-	Signature			string		`json:"signature" gorm:"signature,omitempty"`
-	TotalFavorited		int64		`json:"total_favorited" gorm:"total_favorited,omitempty"`
-	WorkCount			int64		`json:"work_count" gorm:"work_count,omitempty"`
-	FavoriteCount		int64		`json:"favorite_count" gorm:"favorite_count,omitempty"`
-	Relations       	[]*User 	`json:"-" gorm:"many2many:user_relations;association_jointable_foreignkey:follow_id"`    //用户之间的多对多
-	Videos        		[]*Video    `json:"-"`                                    											 //用户与投稿视频的一对多
-	Favorite   			[]*Video    `json:"-" gorm:"many2many:user_favorite;"` 	   											 //用户与点赞视频之间的多对多
-	Comments      		[]*Comment  `json:"-"`                                     											 //用户与评论的一对多
+	ID              int64      `json:"id" gorm:"id,omitempty;primaryKey;AUTO_INCREMENT"`
+	Username        string     `json:"username" gorm:"username,omitempty"`
+	Password        string     `json:"password" gorm:"size:200;notnull"`
+	FollowCount     int64      `json:"follow_count" gorm:"follow_count,omitempty"`
+	FollowerCount   int64      `json:"follower_count" gorm:"follower_count,omitempty"`
+	IsFollow        bool       `json:"is_follow" gorm:"is_follow,omitempty"`
+	Avatar          string     `json:"avatar" gorm:"avatar,omitempty"`
+	BackgroundImage string     `json:"background_image" gorm:"background_image,omitempty"`
+	Signature       string     `json:"signature" gorm:"signature,omitempty"`
+	TotalFavorited  int64      `json:"total_favorited" gorm:"total_favorited,omitempty"`
+	WorkCount       int64      `json:"work_count" gorm:"work_count,omitempty"`
+	FavoriteCount   int64      `json:"favorite_count" gorm:"favorite_count,omitempty"`
+	Relations       []*User    `json:"-" gorm:"many2many:user_relations;association_jointable_foreignkey:follow_id"` //用户之间的多对多
+	Videos          []*Video   `json:"-"`                                                                            //用户与投稿视频的一对多
+	Favorite        []*Video   `json:"-" gorm:"many2many:user_favorite;"`                                            //用户与点赞视频之间的多对多
+	Comments        []*Comment `json:"-"`                                                                            //用户与评论的一对多
 }
 
 // 单例模式
@@ -29,8 +33,8 @@ type UserDAO struct {
 }
 
 var (
-	userDao 	*UserDAO
-	userOnce	sync.Once
+	userDao  *UserDAO
+	userOnce sync.Once
 )
 
 func InitUserDao() *UserDAO {
@@ -41,7 +45,7 @@ func InitUserDao() *UserDAO {
 }
 
 // register
-func (u *UserDAO) IsExistName(name string) bool{
+func (u *UserDAO) IsExistName(name string) bool {
 	var user User
 	DB.Find(&user, "username = ?", name)
 	return user.ID != 0
@@ -55,7 +59,7 @@ func (u *UserDAO) UserRegister(user *User) error {
 // login
 func (u *UserDAO) UserLoginVerify(name string, password string, user *User) bool {
 	DB.First(&user, "username = ? and password = ?", name, password)
-	
+
 	return user.ID == 0
 }
 
@@ -72,10 +76,27 @@ func (u *UserDAO) UserInfoById(userId int64, user *User) error {
 	}
 	return nil
 }
+
 // 用户是否存在
 func (u *UserDAO) IsExistUserId(userid int64) bool {
 	var user User
 	DB.Where("id=?", userid).First(&user)
 
 	return user.ID == 0
+}
+
+func (u *UserDAO) GetFollowerListByUserId(userId int64, userList *[]*User) error {
+	if userList == nil {
+		return errors.New("空指针错误")
+	}
+	var err error
+	if err = DB.Raw("SELECT u.* FROM user_relations r, user_infos u WHERE r.follow_id = ? AND r.user_info_id = u.id", userId).Scan(userList).Error; err != nil {
+		return err
+	}
+
+	if len(*userList) == 0 || (*userList)[0].ID == 0 {
+		return EmptyUserList
+	}
+
+	return nil
 }
